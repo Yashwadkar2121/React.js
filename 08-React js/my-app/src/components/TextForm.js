16f31ca8cc21f3a1f799d0992460684f748f9a7e
Yashwadkar2121 <yashwadkar143@gmail.com>
2023-12-31 01:47:41 +0530

    New Project

---
+++ 08-React js/my-app/src/components/TextForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+  const [text, setText] = useState("Enter the Text");
+  //   text = "new text"; Wrong way to change the state
+  //   setText = ("new text") ; Correct way to cahnge the state
+  return (
+    <>
+      <div>
+        <h1>{props.heading} :-</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            id="myBox"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+      </div>
+      <div className="container my-3 ">
+        <h2>Your Text Summary</h2>
+        <p>
+          <b>{text.split(" ").length}</b> Words and <b>{text.length}</b>{" "}
+          Characters
+        </p>
+        <p>
+          <b>{0.008 * text.split(" ").length}</b> Read
+        </p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
